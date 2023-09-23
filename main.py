6fcd7c9f1a7715321fc7af0941ecc46d66df1b94
Rudio1 <guilhermerudio6@gmail.com>
2023-09-23 14:50:06 -0300

    sorteio nickninja

--- main.py
+++ main.py
@@ -105,5 +105,42 @@ async def sortearconta(ctx):
                 pass
         else:
             await ctx.send(embed=(embed := discord.Embed(title=f'Ops...', description=f'Você não tem autorização para isso.\nPeça para um dos {(roledonos := discord.utils.get(guild.roles, id=1151925408925753394)).mention} para iniciar um sorteio.')), view=(view := Menu()))
+
+
+
+@client.command(name="sorteio") 
+async def sortearnick(ctx):
+    global total_participants
+    global sorteio
+    if ctx.channel.id == 1154178318757797888:
+        guild = client.get_guild(1151924494789779596)
+        rolemod = discord.utils.get(guild.roles, id=1151925408925753394)
+        if rolemod in ctx.author.roles:  
+            try:
+                content = ctx.message.content
+                content = content.replace('!sorteio ', "")
+                nick, n = content.split(',')
+            except:
+                await ctx.send(embed=(embed:= discord.Embed(title=f"Faltam argumentos.",description=f'Exemplo de uso:```!nick Karthus,30\n\nnick: Karthus\ninicio do sorteio: 30 segundos```')), ephemeral=True)
+                return
+
+            message = await ctx.send(embed=(embed := discord.Embed(title=f'Sorteio do nosso Sniper de Nick **{nick}**', description=f'Clique no botão para se registrar no sorteio!\nTotal de Participantes: {total_participants}\nO sorteio será realizado em {n} segundos.')), view=(view := Menu()))
+
+            while int(n) > 0:
+                await message.edit(embed=discord.Embed(title=f'Sorteio do Acesso limitado ao  Sniper de Nick **{nick}**', description=f'Clique no botão para se registrar no sorteio!\nTotal de Participantes: {total_participants}\nO sorteio será realizado em {n} segundos.'))
+                await asyncio.sleep(1) 
+                n = str(int(n) - 1)
+
+            sorteado = client.get_user((sorteadoid := random.choice(sorteio)))
+            await ctx.send(embed=(embed := discord.Embed(title=f"Parabéns!", description=f"{sorteado.mention} venceu o sorteio e faturou o acesso de 3 dias ao sniper de nick!")))
+            await message.delete()
+            total_participants = 0
+            sorteio = []
+            try:
+                await sorteado.send(embed=(embed := discord.Embed(title=f"Ganhou!",description=f"Você se inscreveu e ganhou o sorteio do acesso ao sniper de nick durante 3 dias no servidor da NinjaCorps.\nResgate com:```@rudio1\n@brenoprates\n@gopzera\n@gentlemantrollface```")))
+            except:
+                pass
+        else:
+            await ctx.send(embed=(embed := discord.Embed(title=f'Ops...', description=f'Você não tem autorização para isso.\nPeça para um dos {(roledonos := discord.utils.get(guild.roles, id=1151925408925753394)).mention} para iniciar um sorteio.')), view=(view := Menu()))
           
 client.run('MTE1MzQ5NjQzMzU5MzQyMTg2Nw.G9_Ky4.bDBieUtiu6HfUaDjOQ0bZwfOdt7A-hTbVzo1QA')
